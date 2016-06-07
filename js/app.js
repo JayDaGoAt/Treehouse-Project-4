4c50dc5b0817e09bde1153d0feb78d233d6ffa89
bb2j3z <brian@visitthelab.com>
2016-06-07 17:37:52 -0400

    Implemented initial functionality

---
+++ js/app.js
@@ -0,0 +1,37 @@
+﻿
+var $searchBox = $("#image_search");
+
+
+function searchEvent() {
+
+    var bImageVisible = false;
+
+    $('#imageGallery li a').each(function () {
+        if ($searchBox.val() != "") {
+            var dataString = $(this).attr("data-title");
+            if (dataString != null) {
+
+                if (dataString.indexOf($searchBox.val()) != -1) {
+                    $(this).parent().show();
+                    bImageVisible = true;
+                }
+                else {
+                    $(this).parent().hide();
+                }
+            }
+        }
+        else {
+            $(this).parent().show();
+            bImageVisible = true;
+        }
+    });
+
+    if (!bImageVisible) {
+        $("#imageMessage").show();
+    }
+    else {
+        $("#imageMessage").hide();
+    }
+}
+
+$searchBox.focus(searchEvent).keyup(searchEvent)
